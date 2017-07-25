927dcae331a824d0e9a9f25fd5d4aa126e6c14a7
rameshnittali <rameshnittali123@gmail.com>
2017-07-25 21:09:54 +0530

    User model for login and registration
    this handles user related data functions

--- app/UsersModel.php
+++ app/UsersModel.php
@@ -3,47 +3,29 @@
 namespace App;
 
 use Jenssegers\Mongodb\Model as Eloquent;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Log;
 use Jenssegers\Mongodb\Eloquent\SoftDeletes;
-use App\RoleModel;
 
 class UsersModel extends Eloquent {
 
     protected $collection = 'users';
-    protected $fillable = array('first_name', 'last_name', 'email', 'password', 'contact_number',
-        'resume_text', 'resume_file', 'role_id');
+    protected $fillable = array('username', 'first_name', 'last_name', 'password', 'email');
 
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
     protected $hidden = ['password'];
 
-    
-
-    /* param 1: email 
-      param 2: password
-
-      Response :
-      if user exist it return user data along with count
-      else
-      returns count 0;
-
-     */
-
-    public function aunthenticate($email, $password) {
-        $count = UsersModel::where(array('email' => $email, 'password' => $password))->count();
-        if ($count == 1) {
-            $user_data = $this->user_details($email);
-            $user_data['count'] = $count;
-        } else {
-            $user_data['count'] = $count;
-        }
+    public function aunthenticate($login_data) {
+        $user_data = UsersModel::where($login_data)->first();
         return $user_data;
     }
 
-    public function user_details($email) {
-        $user_data = UsersModel::where(array('email' => $email))->first();
+    public function user_details($user_id = NULL) {
+        if ($user_id != NULL) {
+            $user_data = UsersModel::where($user_id)->first();
+        } else {
+            $user_data = UsersModel::all();
+        }
         return $user_data;
     }
 
