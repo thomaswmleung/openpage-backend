709d242c883b98f746a63bc9614cad649784da5e
rameshnittali <rameshnittali123@gmail.com>
2017-07-28 21:15:11 +0530

    Section 0.1.2 in API sheet :  Handled validations and response will be sent with error description than -1

---
+++ resources/views/emails/activation_mail.php
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<body style="background: black; color: white">
+<h1>Hello <?php echo $username;?>,</h1>
+<p>Welcome to openpage</p>
+<p><?php echo $content;?></p>
+</body>
+</html>
