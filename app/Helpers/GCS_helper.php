d9efec2032762761d5fe168e0ff34d952772ee88
rameshnittali <rameshnittali123@gmail.com>
2017-08-01 18:42:15 +0530

    Ref to 0.1.2 Media REST API's
    Code pertaining to Media REST API is updated

---
+++ app/Helpers/GCS_helper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Helpers;
+
+use Google\Cloud\Storage\StorageClient;
+use Google\Cloud\ServiceBuilder;
+use Illuminate\Support\Facades\Config;
+
+class GCS_helper {
+
+    static public function upload_to_gcs($file_path) {
+        $gcloud = new ServiceBuilder([
+            'keyFilePath' => Config::get('constants.gcs_key'),
+            'projectId' => Config::get('constants.gcs_bucket_name')
+        ]);
+
+        // Fetch an instance of the Storage Client
+        $storage = $gcloud->storage();
+
+        $bucket = $storage->bucket(Config::get('constants.gcs_bucket_name'));
+
+        // Upload a file to the bucket.
+        try {
+            $bucket->upload(
+                    fopen($file_path, 'r'), [
+                'predefinedAcl' => 'publicRead'
+                    ]
+            );
+        } catch (Exception $e) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    
+}
