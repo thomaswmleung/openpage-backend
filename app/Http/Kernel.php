7710a7ed20e722a531b2a94c8efeaa83b3d12522
rameshnittali <rameshnittali123@gmail.com>
2017-08-02 20:40:11 +0530

    Ref: 0.1.2 middleware auth
    Pertaining to middleware auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
         'api' => [
             'throttle:60,1',
             'bindings',
@@ -56,5 +55,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'login_auth' => \App\Http\Middleware\LoginAuth::class,
     ];
 }
