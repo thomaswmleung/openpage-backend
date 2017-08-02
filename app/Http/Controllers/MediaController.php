e695234f406fb6aeea5126c1b82da0a48bbc4c84
rameshnittali <rameshnittali123@gmail.com>
2017-08-02 18:49:58 +0530

    with ref: 0.1.2 - Updated the media api for delete
    Updated media api for delete

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Config;
 class MediaController extends Controller {
 
     public function media(Request $request) {
+        // die(var_dump($request->header('token')));
         $mediaModel = new MediaModel();
         if (isset($request->mid) && $request->mid != "") {
             $media_id = $request->mid;
@@ -41,7 +42,7 @@ class MediaController extends Controller {
             'media_file' => 'required|mimes:jpeg,png,jpg,mp3,ogg,mp4,pdf,zip',
             'owner' => 'required',
             'usage' => 'required',
-            'created_by' => 'required|exists:users,_id',
+                //'created_by' => 'required|exists:users,_id',
         );
         $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
@@ -50,20 +51,20 @@ class MediaController extends Controller {
             $media_array = $request->all();
             if ($request->hasFile('media_file')) {
                 $image = $request->file('media_file');
-                $input['media_file'] = time() .uniqid(). '.' . $image->getClientOriginalExtension();
+                $input['media_file'] = time() . uniqid() . '.' . $image->getClientOriginalExtension();
                 $destinationPath = public_path('images');
                 $media_name = $input['media_file'];
-                $image->move($destinationPath,$media_name );
-               
+                $image->move($destinationPath, $media_name);
+
                 //upload to GCS
-                
-                $gcs_result = GCS_helper::upload_to_gcs('images/'.$media_name);
+
+                $gcs_result = GCS_helper::upload_to_gcs('images/' . $media_name);
                 if (!$gcs_result) {
                     $error['error'] = array("Error in upload of GCS");
                     return response(json_encode($error), 400);
                 }
                 // delete your local pdf file here
-                unlink($destinationPath."/".$media_name);
+                unlink($destinationPath . "/" . $media_name);
 
                 $media_url = "https://storage.googleapis.com/" . Config::get('constants.gcs_bucket_name') . "/" . $media_name;
                 $media_array['url'] = $media_url;
@@ -81,37 +82,39 @@ class MediaController extends Controller {
 //        $media_json = $request->getContent();
 //        $media_array = json_decode($media_json, TRUE);
 //        dd($request->json()->all());
-      
+//
         $rules = array(
             '_id' => 'required|exists:media,_id',
-            'updated_by' => 'required|exists:users,_id',
+                // 'updated_by' => 'required|exists:users,_id',
         );
-        
+
+
         $media_array = array();
-        
+
         $media_array['_id'] = $request->_id;
-        if( isset($request->owner) && $request->owner != ""){
+        if (isset($request->owner) && $request->owner != "") {
             $media_array['owner'] = $request->owner;
         }
-        
-        if( isset($request->right) && $request->right != ""){
+
+        if (isset($request->right) && $request->right != "") {
             $media_array['right'] = $request->right;
         }
-        
-        if( isset($request->usage) && $request->usage != ""){
+
+        if (isset($request->usage) && $request->usage != "") {
             $media_array['usage'] = $request->usage;
         }
-        
-        if( isset($request->updated_by) && $request->updated_by != ""){
-            $media_array['updated_by'] = $request->updated_by;
+
+        if (isset($request->updated_by) && $request->updated_by != "") {
+            //$media_array['updated_by'] = $request->updated_by;
+            $media_array['updated_by'] = $request->header('token');
         }
-      
+
         $validator = Validator::make($media_array, $rules);
         if ($validator->fails()) {
             return response($validator->messages(), 400);
         } else {
             //$media_array = $request->all();
-           
+
             $result = $this->update_media_data($media_array);
             if ($result) {
                 return response("Media updated successfully", 200);
@@ -128,8 +131,20 @@ class MediaController extends Controller {
     }
 
     public function delete_media(Request $request) {
+
         $media_id = trim($request->mid);
-        // TODO
+        $mediaModel = new MediaModel();
+        $media_data = $mediaModel->media_details(array('_id' => $media_id));
+        $data = explode("/", $media_data['url']); // fetching file name from URL
+        $objectName = end($data);
+        $gcs_result = GCS_helper::delete_from_gcs($objectName);
+        if ($gcs_result) {
+            MediaModel::destroy($media_id);
+            return response("Media Deleted successfully", 200);
+        } else {
+            $error['error'] = array("Something went wrong");
+            return response(json_encode($error), 400);
+        }
     }
 
 }
