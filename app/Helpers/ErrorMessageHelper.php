dddc3742104483dbe2de3c8a03ea0872038ed60c
Suraj <surajde16@gmail.com>
2017-09-14 21:05:58 +0530

    Handled the bug fixes raised by the UI team.

--- app/Helpers/ErrorMessageHelper.php
+++ app/Helpers/ErrorMessageHelper.php
@@ -19,6 +19,7 @@ class ErrorMessageHelper {
         }
         return $messages;
     }
+
 //    public static function formulateErrorMessages($err_msgs_array) {
 //
 //        $messages = array();
@@ -34,20 +35,23 @@ class ErrorMessageHelper {
         $errors_array = $errors->messages();
         $response_error_array = array();
         $error_index = 0;
-        
-       
+
+
         foreach ($errors_array as $key => $err_msgs_array) {
 
-    
+
             foreach ($err_msgs_array as $err_details_json) {
 //                var_dump($err_details_json);
 //                exit();
                 $error_detail_array = json_decode($err_details_json, true);
-
-                foreach ($error_detail_array as $code => $msg) {
-
-                    $response_error_array[$error_index]['ERR_CODE'] = $code;
-                    $response_error_array[$error_index]['ERR_MSG'] = $msg;
+                if (is_array($error_detail_array)) {
+                    foreach ($error_detail_array as $code => $msg) {
+                        $response_error_array[$error_index]['ERR_CODE'] = $code;
+                        $response_error_array[$error_index]['ERR_MSG'] = $msg;
+                    }
+                }else{
+                     $response_error_array[$error_index]['ERR_CODE'] = "";
+                     $response_error_array[$error_index]['ERR_MSG'] = $err_details_json;
                 }
             }
             $error_index++;
